db51702b63631d453b734638a4a37901c5299472
谭文斌 <tanwenbin@talkweb.com.cn>
2020-09-08 10:31:58 +0800

    add compile-by-require

--- src/router/index.js
+++ src/router/index.js
@@ -5,13 +5,21 @@ Vue.use(VueRouter);
 
 let routes = [];
 
-const routerContext = require.context("@/views", true, /router\.js$/);
-routerContext.keys().forEach(route => {
-  const routerModule = routerContext(route);
+if (process.env.VUE_APP_MODULE) {
+  routes.push(...require("../views/misc/router.js").default);
+  routes.push(...require("../views/login/router.js").default);
+  routes.push(
+    ...require(`../views/${process.env.VUE_APP_MODULE}/router.js`).default
+  );
+} else {
+  const routerContext = require.context("@/views", true, /router\.js$/);
+  routerContext.keys().forEach(route => {
+    const routerModule = routerContext(route);
 
-  //兼容 import+export 和 require+module.export
-  routes.push(...(routerModule.default || routerModule));
-});
+    //兼容 import+export 和 require+module.export
+    routes.push(...(routerModule.default || routerModule));
+  });
+}
 
 const router = new VueRouter({
   mode: "history",
